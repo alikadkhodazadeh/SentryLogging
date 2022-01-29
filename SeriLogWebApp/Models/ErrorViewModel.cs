34a0e8ca64a262cb66909296722e8b43326fe47f
ali.kdz80 <ali.kdz80@gmail.com>
2022-01-29 14:26:05 +0330

    Create webapp

---
+++ SeriLogWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SeriLogWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
